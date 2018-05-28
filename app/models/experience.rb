9628881c718847d805fee67f0734a5b71d36f095
Swen Kunzel <swenkuenzel@gmail.com>
2018-05-28 17:34:38 +0100

    added booking model and table references

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,2 +1,4 @@
 class Experience < ApplicationRecord
+  belongs_to :user
+  has_many :bookings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_28_160933) do
+ActiveRecord::Schema.define(version: 2018_05_28_163238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "experience_id"
+    t.string "date"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["experience_id"], name: "index_bookings_on_experience_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "experiences", force: :cascade do |t|
     t.string "address"
@@ -47,4 +58,6 @@ ActiveRecord::Schema.define(version: 2018_05_28_160933) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "experiences"
+  add_foreign_key "bookings", "users"
 end
