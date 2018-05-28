d57f6ad0ec69e3cd0474e04236e4d7230cf27b0b
Swen Kunzel <swenkuenzel@gmail.com>
2018-05-28 17:01:30 +0100

    experiences routes and controller added

---
+++ app/controllers/experiences_controller.rb
@@ -0,0 +1,26 @@
+class ExperiencesController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  private
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_05_28_144855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "experiences", force: :cascade do |t|
+    t.string "address"
+    t.string "title"
+    t.text "description"
+    t.float "price"
+    t.float "duration"
+    t.string "category"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
