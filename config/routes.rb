5e205e530e91391fa2f5177b7224d3f57d33aa29
Swen Kunzel <swenkuenzel@gmail.com>
2018-05-28 17:41:00 +0100

    booking routes and controller and new view added

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   root to: 'experiences#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :experiences
+  resources :experiences do
+    resources :bookings, only: [:new, :create, :delete]
+  end
 
 end
