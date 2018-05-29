9fd66173b571f0331ea584271e5144ff6bb1a0a8
Swen Kunzel <swenkuenzel@gmail.com>
2018-05-29 17:44:17 +0100

    index page with seeded data

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -2,6 +2,7 @@ class ExperiencesController < ApplicationController
   before_action :set_experience, only: [:show, :edit, :update, :destroy]
 
   def index
+    @experiences = Experience.all.limit(15)
   end
 
   def show
