f6c0a2fcb08e36804275b26e3cd7c8692abc4419
David Zwagemaker <david@zwagemaker.nl>
2018-05-29 12:52:00 +0100

    Added DB seeds and photo column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_073115) do
+ActiveRecord::Schema.define(version: 2018_05_29_112130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,10 @@ ActiveRecord::Schema.define(version: 2018_05_29_073115) do
   create_table "bookings", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "experience_id"
-    t.string "date"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "date_time"
     t.index ["experience_id"], name: "index_bookings_on_experience_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_05_29_073115) do
     t.string "first_name"
     t.string "last_name"
     t.string "phone"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
