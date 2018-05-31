a15001dfa24646379bed5ee06887c4c94015a949
Emily Burns <emily5001@gmail.com>
2018-05-31 18:10:22 +0100

    removed the string tags

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,11 @@
 import "bootstrap";
-import './application.css'
+import './application.css';
+
+import Slider from '../components/bootstrap-slider.js';
+
 import initDatepickers from '../components/datepicker';
-
-//= require simple_form_extension
-
 initDatepickers();
+
+
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
